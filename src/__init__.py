fc2713f426b92043de67c1b1924ac750a8771201
Zacharia Ndung'u <ndunguzachary24@gmail.com>
2024-06-05 23:05:12 +0300

    User registration

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
-from decouple import config
+from decouple import config 
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
-from src.user.services.user import RegisterUser
+from flask_restful import Api 
+from user.services.user import RegisterUser
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
